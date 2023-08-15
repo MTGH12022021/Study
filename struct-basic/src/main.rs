1afc5def0a8e95b5b52708fef1a1d8d5801c5ae6
ntl <ntloi120202@gmail.com>
2023-08-15 16:22:44 +0700

    [Done] enum basic

--- struct-basic/src/main.rs
+++ struct-basic/src/main.rs
@@ -1,51 +1,65 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
+// //todo struct
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
 
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
+// impl Rectangle {
+//     fn area(&self) -> u32 {
+//         self.width * self.height
+//     }
 
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
+//     fn can_hold(&self, other: &Rectangle) -> bool {
+//         self.width > other.width && self.height > other.height
+//     }
 
-    fn square(size: u32) -> Self {
-        Self {
-            width: size,
-            height: size,
-        }
-    }
+//     fn square(size: u32) -> Self {
+//         Self {
+//             width: size,
+//             height: size,
+//         }
+//     }
 
-    fn set_with(&mut self) {
-        self.width = 21;
-    }
+//     fn set_with(&mut self) {
+//         self.width = 21;
+//     }
 
-    fn get_with(&self) -> u32 {
-        self.width
-    }
-}
+//     fn get_with(&self) -> u32 {
+//         self.width
+//     }
+// }
+
+// fn main() {
+//     let mut rect1 = Rectangle {
+//         width: 30,
+//         height: 50,
+//     };
+
+//     println!(
+//         "The area of the rectangle with width: {} is {} square pixels.",
+//         rect1.get_with(),
+//         rect1.area()
+//     );
+
+//     rect1.set_with();
+//     let square: Rectangle = Rectangle::square(12);
+//     println!(
+//         "The area of the rectangle with width: {} is {} square pixels.",
+//         rect1.get_with(),
+//         rect1.area()
+//     );
+// }
+
+//todo enum
 
 fn main() {
-    // let mut rect1 = Rectangle {
-    //     width: 30,
-    //     height: 50,
-    // };
-
-    // println!(
-    //     "The area of the rectangle with width: {} is {} square pixels.",
-    //     rect1.get_with(),
-    //     rect1.area()
-    // );
-
-    // rect1.set_with();
-    // let square: Rectangle = Rectangle::square(12);
-    // println!(
-    //     "The area of the rectangle with width: {} is {} square pixels.",
-    //     rect1.get_with(),
-    //     rect1.area()
-    // );
+    #[derive(Debug)]
+    enum Phone {
+        Iphone,
+        SamSung(String),
+        _Readme(String),
+    }
+    print!("{:?}", Phone::Iphone);
+    print!("{:?}", Phone::SamSung(String::from("SamSung")));
 }
