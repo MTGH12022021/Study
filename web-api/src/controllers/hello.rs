b95160b48c585395f85db2ef16ed32e0a643a23b
ntl <ntloi120202@gmail.com>
2023-08-17 16:30:49 +0700

    [Study] query params with rust api

--- web-api/src/controllers/hello.rs
+++ web-api/src/controllers/hello.rs
@@ -1,17 +1,15 @@
 use actix_web::{web, HttpResponse};
 use serde::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize, Clone)]
-struct MyData {
+pub struct MyData {
     name: String,
     age: u32,
-    parameter: Option<u32>,
 }
 
 pub async fn hello() -> HttpResponse {
     let my_data = MyData {
         name: "My".to_string(),
         age: 30,
-        parameter: None,
     };
     HttpResponse::Ok().json(my_data)
 }
@@ -21,7 +19,14 @@ pub async fn parameter_hello(path: web::Path<(u32, String)>) -> HttpResponse {
     let my_data = MyData {
         name: name,
         age: age,
-        parameter: None,
+    };
+    HttpResponse::Ok().json(my_data)
+}
+
+pub async fn query_params_hello(query_params: web::Query<MyData>) -> HttpResponse {
+    let my_data = MyData {
+        name: query_params.name.clone(),
+        age: query_params.age,
     };
     HttpResponse::Ok().json(my_data)
 }
--- web-api/src/route.rs
+++ web-api/src/route.rs
@@ -5,6 +5,7 @@ pub fn routing(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("")
             .route("/hello", web::get().to(hello::hello))
+            .route("/hello_params", web::get().to(hello::query_params_hello))
             .route("/{hello_age}/{name}", web::get().to(hello::parameter_hello)),
     );
 }
