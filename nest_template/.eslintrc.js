2c68c74d8b6145f3b11d5c03ef1f00752c3a24c6
Nguyễn Tiến Lợi <ntloi120202@gmail.com>
2024-02-24 20:40:16 +0700

    ---Config--- setup format for template

--- nest_template/.eslintrc.js
+++ nest_template/.eslintrc.js
@@ -1,25 +1,33 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: 'tsconfig.json',
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint/eslint-plugin'],
+    "extends": "./tsconfig.json",
+    // extends: ['prettier'],
+    overrides: [
+        {
+            files: ['**/*.js'],
+            excludedFiles: ["node_modules", "dist", "coverage"],
+        },
+    ],    root: true,
+    env: {
+        node: true,
+        jest: true,
+    },
+    rules: {
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        'semi': ['error', 'always'],
+        'semi-spacing': ['error', { 'before': false, 'after': true }],
+        'newline-before-return': 'error',
+        'simple-import-sort/imports': 'error',
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        'indent': 'error',
+    }
 };
